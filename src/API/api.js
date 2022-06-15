a84f831591e5f6383fde2e8822f09b2e2c086644
Justin5s <round_caption0k@icloud.com>
2022-06-15 09:43:15 +0300

    fetch works

--- src/API/api.js
+++ src/API/api.js
@@ -1,4 +1,6 @@
 async function getPetsData() {
+    // console.log('Data123 ===');
+    // return
   const apiResponse = await fetch('https://glittery-dull-snickerdoodle.glitch.me/v1/pets');
   const petsData = await apiResponse.json();
   console.log('petsData ===', petsData);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { getPetsData } from './API/api.js';
 import './App.css';
 // import Card from './components/Card/Card';
 import CardList from './components/CardList/CardList';
 import Header from './components/header/Header';
-import { getPetsData } from './API/api.js';
+
 // import MyButton from './components/MyButton/MyButton';
 
 
 function App() {
   useEffect(() => {
-    const dataList = async () => await getPetsData() 
-    console.log('dataList ===', dataList);
+   async function dataList() { return await getPetsData() }
+    console.log('dataList ===', dataList());
   }, [] )
   return (
     <div className='App'>
       <Header />
       <CardList data={[]} />
-      <footer>
+      {/* <footer>
         <p>Copyright &copy; VetBee 2022 all rights reserved</p>
-      </footer>
+      </footer> */}
     </div>
   );
 }
